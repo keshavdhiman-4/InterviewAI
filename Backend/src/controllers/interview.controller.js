6296de011a8cb8c99ea6e3903d65c7a304d17ce2
keshavdhiman-4 <keshavdhiman167@gmail.com>
2026-08-17 10:19:18 +0530

    Add Puppeteer sandbox flags for Linux deployment

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -3,8 +3,6 @@ const {generateInterviewReport, generateResumePdf} = require("../services/ai.ser
 const interviewReportModel = require("../models/interviewReport.model")
 
 
-
-
 /**
  * @description Controller to generate interview report based on user self description, resume and job description.
  */
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -97,7 +97,15 @@ Job Description: ${jobDescription}`;
 
 
 async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        headless: true, // or "new" depending on your Puppeteer version
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ]
+    });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
